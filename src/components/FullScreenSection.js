c1b4f5853bd4cf5403976c9daab0b6fdfc0e9b18
Shrestha Reewaj <r.waj555@gmail.com>
2024-03-27 20:03:38 +0545

    removed unused json

--- src/components/FullScreenSection.js
+++ src/components/FullScreenSection.js
@@ -18,39 +18,3 @@ const FullScreenSection = ({ children, isDarkBackground, ...boxProps }) => {
 };
 
 export default FullScreenSection;
-
-{
-  $project: {
-    _id: "$quiz._id",
-    name: "$quiz.name",
-    type: {
-      $cond: {
-        if: { $ifNull: ["$quiz.attemptsLimit", false] },
-        then: "quiz",
-        else: "assignment",
-      },
-    },
-    totalMarks: "$quiz.totalMarks",
-    submissions: "$quiz.submissions",
-    finalSubmission: "$quizSubmission.finalSubmission",
-    totalPoints: "$quiz.totalPoints",
-    startDate: "$quiz.startDate",
-    endDate: "$quiz.endDate",
-    description: "$quiz.description",
-    status: {
-      $switch: {
-        branches: [
-          {
-            case: { $lt: ["$quiz.endDate", new Date()] },
-            then: "overdue",
-          },
-          {
-            case: { $gt: ["$quiz.startDate", new Date()] },
-            then: "upcoming",
-          },
-        ],
-        default: "ongoing",
-      },
-    },
-  },
-},
